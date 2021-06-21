5ea557ce79d03fc70a8bbf9ee1b0e8f923a5d5df
Terando <7394068+Terando@users.noreply.github.com>
2021-06-22 00:24:42 +0200

    Update structure and useage on fix.js
    Made some changes to the overall structure of the file to separate them into functions and make some things more efficient and re-useable. Also applied some formatting for better readablity of the file.

--- fix.js
+++ fix.js
@@ -1,40 +1,51 @@
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
 function storeViewedVideos() {
   if (sessionStorage.viewedItems && localStorage.dumpertPlusPlusViewedItems) {
     const sessionViewedItemsArray = sessionStorage.viewedItems.split(',')
     const localViewedItemsArray = localStorage.dumpertPlusPlusViewedItems.split(',')
-    const mergedArray = localViewedItemsArray.concat(sessionViewedItemsArray)
-    const uniqueArray = mergedArray.filter(onlyUnique)
-    const uniqueString = uniqueArray.toString()
-    localStorage.dumpertPlusPlusViewedItems = uniqueString
+    localStorage.dumpertPlusPlusViewedItems = mergeArraysToString(sessionViewedItemsArray, localViewedItemsArray)
     sessionStorage.viewedItems = localStorage.dumpertPlusPlusViewedItems
   }
   
+  syncViewedItems()
+}
+
+function mergeArraysToString(viewItems, localItems){
+  return [...new Set([...viewItems, ...localItems])].toString()
+}
+
+function syncViewedItems(){
   if (sessionStorage.viewedItems && !localStorage.dumpertPlusPlusViewedItems) {
     localStorage.dumpertPlusPlusViewedItems = sessionStorage.viewedItems
   }
-  
+
   if (!sessionStorage.viewedItems && localStorage.dumpertPlusPlusViewedItems) {
     sessionStorage.viewedItems = localStorage.dumpertPlusPlusViewedItems
   }
 }
 
 function scrollToLocation(){
-  window.scrollTo(0,  sessionStorage.dumpertPlusPlusScrollLocation)
+  window.scroll({
+    top: getScrollLocation(),
+    left: 0,
+    behavior: 'auto'
+  })
 }
 
 function setScrollLocation(){
-  const scroll = this.scrollY
-  if(scroll != 0) {
-    sessionStorage.dumpertPlusPlusScrollLocation = scroll
+  if (this.scrollY !== 0) {
+    sessionStorage.dumpertPlusPlusScrollLocation = this.scrollY
   }
 }
+
+function getScrollLocation(){
+  return sessionStorage.dumpertPlusPlusScrollLocation ? sessionStorage.dumpertPlusPlusScrollLocation : 1
+}
+
 window.addEventListener('beforeunload', storeViewedVideos)
 window.addEventListener('click', storeViewedVideos)
-window.addEventListener("scroll", setScrollLocation)
+window.addEventListener('scroll', setScrollLocation)
 window.addEventListener('fullscreenchange', scrollToLocation)
+
 storeViewedVideos()
+
 // console.log('dikke tieten')
